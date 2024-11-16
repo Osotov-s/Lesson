ef8838de1d3212680a393c3b7d01e6d650ea2a59
Osotov-s <82163862+Osotov-s@users.noreply.github.com>
2024-11-16 22:05:18 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+Number_of_completed_tasks = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = (Number_of_hours_spent / Number_of_completed_tasks)
+print( Number_of_completed_tasks,'заданий было выполнено',Number_of_hours_spent,'часа было затрачено на изучение курса',Course_name,Time_for_one_task,'часа занимает одно задание')
