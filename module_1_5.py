d3d0c4b86deaf940143e9dadb74912286f961ed3
Osotov-s <82163862+Osotov-s@users.noreply.github.com>
2024-11-21 19:41:44 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+
+
+immutable_var = 1, True, "String",
+print(immutable_var)
+#immutable_var[0] = 4 #кортеж не поддерживает обращение по (элементам.
+#print(immutable_var))
+
+mutable_list = [2, False, "Int"]
+print(mutable_list)
+mutable_list[0] = 5
+print(mutable_list)
